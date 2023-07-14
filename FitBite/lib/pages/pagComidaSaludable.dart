1d0048df869102e5a293918e08d5f8dd8c0f0945
JuanMartinDominguez <85463091+JuanMartinDominguez@users.noreply.github.com>
2023-07-14 18:48:00 -0500

    Subida página comida saludable

---
+++ FitBite/lib/pages/pagComidaSaludable.dart
@@ -0,0 +1,276 @@
+import 'package:fitbite/pages/pagHome.dart';
+import 'package:fitbite/pages/pagRecomendacionSaludable.dart';
+import 'package:fitbite/pages/pagResultadoComidas.dart';
+import 'package:fitbite/pages/popupValorNutricional.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Comidas Saludables',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: PagComidaSaludable(),
+    );
+  }
+}
+
+class PagComidaSaludable extends StatelessWidget {
+  void _mostrarPopup(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return FractionallySizedBox(
+          widthFactor: 0.8,
+          child: AlertDialog(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Información Nutricional',
+                  style: TextStyle(color: Color(0xFF0CA5B0)),
+                ),
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Calorías: 100'),
+                Text('Carbohidratos: 20g'),
+                Text('Grasas: 5g'),
+                Text('Proteínas: 10g'),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _mostrarPopupGuardado(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return FractionallySizedBox(
+          widthFactor: 0.8,
+          child: AlertDialog(
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Comida Guardada',
+                  style: TextStyle(color: Color(0xFF0CA5B0)),
+                ),
+                IconButton(
+                  icon: Icon(Icons.close),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+            content: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text('Esta comida ha sido guardada'),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        automaticallyImplyLeading: true,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => ResultadoComidasScreen()),
+            );
+          },
+          color: Colors.black,
+        ),
+      ),
+      body: Container(
+        color: Colors.white,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(height: 16.0),
+                Container(
+                  alignment: Alignment.center,
+                  child: Text(
+                    'Receta saludable',
+                    style: TextStyle(
+                      fontSize: 32.0,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF0CA5B0),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30.0),
+                Row(
+                  children: [
+                    Text(
+                      'Desayuno',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Spacer(),
+                    GestureDetector(
+                      onTap: () {
+                        _mostrarPopupGuardado(context); // Llamada al método del popup
+                      },
+                      child: Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          icon: Icon(Icons.bookmark_border_outlined),
+                          onPressed: () {
+                            _mostrarPopupGuardado(context);
+                          },
+                        ),
+                      ),
+                    ),
+                    GestureDetector(
+                      onTap: () {
+                        _mostrarPopup(context); // Llamada al método del popup
+                      },
+                      child: Material(
+                        color: Colors.transparent,
+                        child: IconButton(
+                          icon: Icon(Icons.info_outline),
+                          onPressed: () {
+                            _mostrarPopup(context);
+                          },
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 16.0),
+                Container(
+                  height: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 0.3,
+                    ),
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20.0),
+                    child: Container(
+                      padding: EdgeInsets.all(16.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: Container(
+                              child: Image.asset(
+                                'assets/img/recomendacion_saludable.jpg',
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                          Expanded(
+                            flex: 2,
+                            child: Container(
+                              padding: EdgeInsets.all(16.0),
+                              child: Center(
+                                child: Text(
+                                  'Comida Saludable',
+                                  style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 16.0),
+                Text(
+                  'Ingredientes',
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Texto de ingredientes',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 300.0),
+              ],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.white,
+        child: Container(
+          padding: EdgeInsets.symmetric(vertical: 8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              GestureDetector(
+                onTap: () {
+                  // Acción para el ícono de inicio
+                },
+                child: Icon(Icons.home, size: 27.0, color: Color.fromRGBO(12, 165, 176, 0.75)),
+              ),
+              GestureDetector(
+                onTap: () {
+                  // Acción para el ícono de cámara
+                },
+                child: Icon(Icons.camera_alt, size: 27.0, color: Color(0XFF0CA5B0)),
+              ),
+              GestureDetector(
+                onTap: () {
+                  // Acción para el ícono de perfil
+                },
+                child: Icon(Icons.person, size: 27.0, color: Color.fromRGBO(12, 165, 176, 0.75)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
